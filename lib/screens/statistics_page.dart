e753553d8446bccd4f537931e29639aa3d824842
Marcin Szalek <marcin.szalek1@gmail.com>
2017-10-19 17:15:45 +0200

    Added more statistics cards

--- lib/screens/statistics_page.dart
+++ lib/screens/statistics_page.dart
@@ -6,8 +6,13 @@ import 'package:weight_tracker/model/weight_entry.dart';
 class _StatisticsPageViewModel {
   final double totalProgress;
   final double currentWeight;
+  final double last7daysProgress;
+  final double last30daysProgress;
 
-  _StatisticsPageViewModel({this.totalProgress, this.currentWeight});
+  _StatisticsPageViewModel({this.last7daysProgress,
+    this.last30daysProgress,
+    this.totalProgress,
+    this.currentWeight});
 }
 
 class StatisticsPage extends StatelessWidget {
@@ -15,12 +20,29 @@ class StatisticsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return new StoreConnector<ReduxState, _StatisticsPageViewModel>(
       converter: (store) {
-        List<WeightEntry> entries = store.state.entries;
+        List<WeightEntry> entries = new List.from(store.state.entries);
+        List<WeightEntry> last7daysEntries = entries.where((entry) =>
+            entry
+                .dateTime
+                .isAfter(new DateTime.now().subtract(new Duration(days: 7))))
+            .toList();
+        List<WeightEntry> last30daysEntries = entries.where((entry) =>
+            entry
+                .dateTime
+                .isAfter(new DateTime.now().subtract(new Duration(days: 30))))
+            .toList();
         return new _StatisticsPageViewModel(
           totalProgress: entries.isEmpty
               ? 0.0
               : (entries.first.weight - entries.last.weight),
           currentWeight: entries.isEmpty ? 0.0 : entries.first.weight,
+          last7daysProgress: last7daysEntries.isEmpty
+              ? 0.0
+              : (last7daysEntries.first.weight - last7daysEntries.last.weight),
+          last30daysProgress: last30daysEntries.isEmpty
+              ? 0.0
+              : (last30daysEntries.first.weight -
+              last30daysEntries.last.weight),
         );
       },
       builder: (context, viewModel) {
@@ -35,6 +57,27 @@ class StatisticsPage extends StatelessWidget {
               value: viewModel.totalProgress,
               processNumberSymbol: true,
             ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new Expanded(
+                  child: new _StatisticCard(
+                    title: "Last week",
+                    value: viewModel.last7daysProgress,
+                    textSizeFactor: 0.8,
+                    processNumberSymbol: true,
+                  ),
+                ),
+                new Expanded(
+                  child: new _StatisticCard(
+                    title: "Last month",
+                    value: viewModel.last30daysProgress,
+                    textSizeFactor: 0.8,
+                    processNumberSymbol: true,
+                  ),
+                ),
+              ],
+            )
           ],
         );
       },
@@ -67,14 +110,16 @@ class _StatisticCard extends StatelessWidget {
   final String title;
   final num value;
   final bool processNumberSymbol;
+  final double textSizeFactor;
 
-  _StatisticCard({this.title, this.value, this.processNumberSymbol = false});
+  _StatisticCard(
+      {this.title, this.value, this.processNumberSymbol = false, this.textSizeFactor = 1.0});
 
   @override
   Widget build(BuildContext context) {
     Color numberColor =
         (processNumberSymbol && value > 0) ? Colors.red : Colors.green;
-    String numberSymbol = processNumberSymbol ? (value < 0 ? "-" : "+") : "";
+    String numberSymbol = processNumberSymbol && value > 0 ? "+" : "";
     return new _StatisticCardWrapper(
       child: new Column(
         children: <Widget>[
@@ -83,6 +128,7 @@ class _StatisticCard extends StatelessWidget {
               children: [
                 new Text(
                   numberSymbol + value.toString(),
+                  textScaleFactor: textSizeFactor,
                   style: Theme
                       .of(context)
                       .textTheme
