53c34e949532e7f519ecbe52f310b5cfc03feb06
Marcin Szalek <marcin.szalek1@gmail.com>
2017-12-06 10:12:22 +0100

    name changes

--- test/widget_tests/main_page_tests.dart
+++ test/widget_tests/main_page_tests.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:weight_tracker/main.dart';
 
 void main() {
-  testWidgets('my first widget test', (WidgetTester tester) async {
+  testWidgets('App name in header', (WidgetTester tester) async {
     await tester.pumpWidget(new MyApp());
     expect(find.text('Weight Tracker'), findsOneWidget);
   });
